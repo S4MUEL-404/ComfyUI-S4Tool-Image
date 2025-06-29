7a7ca50d2dc50238a505ec2bf0e6efc6aba2897a
S4MUEL <im@s4muel.com>
2025-06-29 11:48:39 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,6 +10,7 @@ from .ImagePalette import ImagePalette
 from .ImagePalette631 import ImagePalette631
 from .ImagePrimaryColor import ImagePrimaryColor
 from .ImageFromBase64 import ImageFromBase64
+from .ImageToBase64 import ImageToBase64
 from .ImageTilingPattern import ImageTilingPattern
 
 NODE_CLASS_MAPPINGS = {
@@ -23,12 +24,13 @@ NODE_CLASS_MAPPINGS = {
     "ImagePalette631": ImagePalette631,
     "ImagePrimaryColor": ImagePrimaryColor,
     "ImageFromBase64": ImageFromBase64,
+    "ImageToBase64": ImageToBase64,
     "ImageTilingPattern": ImageTilingPattern
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ImageCombine": "💀Image Combine",
-    "ImageBlendWithAlpha": "💀Image Blend with Alpha",
+    "ImageBlendWithAlpha": "💀Image Blend With Alpha",
     "ImageMaskExpand": "💀Image Mask Expand",
     "ImageCropToFit": "💀Image Crop To Fit",
     "ImageColor": "💀Image Color",
@@ -37,5 +39,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "ImagePalette631": "💀Image Palette 6-3-1",
     "ImagePrimaryColor": "💀Image Primary Color",
     "ImageFromBase64": "💀Image from Base64",
+    "ImageToBase64": "💀Image To Base64",
     "ImageTilingPattern": "💀Image Tiling Pattern"
 }
